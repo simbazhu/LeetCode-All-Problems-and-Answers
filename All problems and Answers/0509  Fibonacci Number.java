d42760d2891d2410ca850ec2e54f3c9f04522b98
Yifan Zhu <zhuyi@tcd.ie>
2020-01-06 10:30:51 +0000

    Create 0509  Fibonacci Number.java

---
+++ All problems and Answers/0509  Fibonacci Number.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int fib(int N) {
+        if (N <= 1) {
+            return N;
+        }
+        int[][] A = new int[][]{{1, 1}, {1, 0}};
+        matrixPower(A, N-1);
+        
+        return A[0][0];
+    }
+    
+    void matrixPower(int[][] A, int N) {
+        if (N <= 1) {
+            return ;
+        }
+        matrixPower(A, N/2);
+        multiply(A, A);
+        
+        int[][] B = new int[][]{{1, 1}, {1, 0}};
+        if (N % 2 != 0) {
+            multiply(A, B);
+        }
+    }
+    
+    void multiply(int[][] A, int[][] B) {
+        int x = A[0][0] * B[0][0] + A[0][1] * B[1][0];
+        int y = A[0][0] * B[0][1] + A[0][1] * B[1][1];
+        int z = A[1][0] * B[0][0] + A[1][1] * B[1][0];
+        int w = A[1][0] * B[0][1] + A[1][1] * B[1][1];
+        
+        A[0][0] = x;
+        A[0][1] = y;
+        A[1][0] = z;
+        A[1][1] = w;
+    }
+}
