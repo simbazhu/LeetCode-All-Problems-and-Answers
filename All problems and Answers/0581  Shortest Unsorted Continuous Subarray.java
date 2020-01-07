dbda97c3be6a9c9e732d8ab88ab268f410853ede
Yifan Zhu <zhuyi@tcd.ie>
2020-01-07 10:21:15 +0000

    Create 0581  Shortest Unsorted Continuous Subarray.java

---
+++ All problems and Answers/0581  Shortest Unsorted Continuous Subarray.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        boolean flag = false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1])
+                flag = true;
+            if (flag)
+                min = Math.min(min, nums[i]);
+        }
+        flag = false;
+        for (int j = nums.length - 2; j >= 0; j--) {
+            if (nums[j] > nums[j + 1])
+                flag = true;
+            if (flag)
+                max = Math.max(max, nums[j]);
+        }
+        int l, r;
+        for (l = 0; l < nums.length; l++) {
+            if (min < nums[l])
+                break;
+        }
+        for (r = nums.length - 1; r >= 0; r--) {
+            if (max > nums[r])
+                break;
+        }
+        return r - l < 0 ? 0 : r - l + 1;
+    }
+}
