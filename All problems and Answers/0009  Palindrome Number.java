d58b3a215877246451409bbc33e3f02d958d9e3b
Yifan Zhu <zhuyi@tcd.ie>
2020-02-16 16:02:43 +0000

    Create 0009  Palindrome Number.java

---
+++ All problems and Answers/0009  Palindrome Number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Special cases:
+        // When x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, in order to be a palindrome,
+        // the first digit of the number also needs to be 0.
+        // Only 0 satisfy this property.
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        
+        // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
