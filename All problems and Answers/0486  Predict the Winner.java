5f758a05bcb5cd245d703ef0f4d06eef53d9b553
Yifan Zhu <zhuyi@tcd.ie>
2020-06-11 00:38:15 +0100

    Create 0486  Predict the Winner.java

---
+++ All problems and Answers/0486  Predict the Winner.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        int[] dp = new int[nums.length];
+        for (int s = nums.length - 1; s >= 0; s--) {
+            dp[s] = nums[s];
+            for (int e = s + 1; e < nums.length; e++) {
+                int a = nums[s] - dp[e];
+                int b = nums[e] - dp[e - 1];
+                dp[e] = Math.max(a, b);
+            }
+        }
+        return dp[nums.length - 1] >= 0;
+    }
+}
