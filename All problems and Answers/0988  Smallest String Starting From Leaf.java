27c2b06563a4896d5e8dadda07b89c9765d33606
Yifan Zhu <zhuyi@tcd.ie>
2020-06-19 16:27:38 +0100

    Create 0988  Smallest String Starting From Leaf.java

---
+++ All problems and Answers/0988  Smallest String Starting From Leaf.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    String ans = "~";
+    public String smallestFromLeaf(TreeNode root) {
+        dfs(root, new StringBuilder());
+        return ans;
+    }
+
+    public void dfs(TreeNode node, StringBuilder sb) {
+        if (node == null) return;
+        sb.append((char)('a' + node.val));
+
+        if (node.left == null && node.right == null) {
+            sb.reverse();
+            String S = sb.toString();
+            sb.reverse();
+
+            if (S.compareTo(ans) < 0)
+                ans = S;
+        }
+
+        dfs(node.left, sb);
+        dfs(node.right, sb);
+        sb.deleteCharAt(sb.length() - 1);
+    }
+}
