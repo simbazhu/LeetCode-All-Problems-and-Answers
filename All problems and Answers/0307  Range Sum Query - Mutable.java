3c22d6b82cfb98125afba6ebf257b3877c7f7ceb
Yifan Zhu <zhuyi@tcd.ie>
2020-06-21 21:49:59 +0100

    Create 0307  Range Sum Query - Mutable.java

---
+++ All problems and Answers/0307  Range Sum Query - Mutable.java
@@ -0,0 +1,27 @@
+class NumArray {
+
+    private int[] nums;
+    
+    public NumArray(int[] nums) {
+        this.nums = nums;
+    }
+    
+    public void update(int i, int val) {
+        nums[i] = val;
+    }
+    
+    public int sumRange(int i, int j) {
+        int sum = 0;
+        for (int l = i; l <= j; l++) {
+            sum += nums[l];
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
