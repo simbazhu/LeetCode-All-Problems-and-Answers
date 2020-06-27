30579d6767cc9af0c9747ce293f6d5bfbe76057e
Yifan Zhu <zhuyi@tcd.ie>
2020-06-27 17:01:09 +0100

    Create 0290  Word Pattern.java

---
+++ All problems and Answers/0290  Word Pattern.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] arr= str.split(" ");
+        HashMap<Character, String> map = new HashMap<Character, String>();
+        Set<String> set = new HashSet<String>();
+        if(arr.length!= pattern.length())
+            return false;
+        for(int i=0; i<arr.length; i++){
+            char c = pattern.charAt(i);
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(arr[i]))
+                    return false;
+            }else{
+                if(set.contains(arr[i]))
+                    return false;
+                map.put(c, arr[i]);
+                set.add(arr[i]);
+            }    
+        }
+        return true;
+    }
+}
