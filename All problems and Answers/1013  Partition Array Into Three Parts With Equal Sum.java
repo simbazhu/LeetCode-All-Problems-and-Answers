d92c544058b5255f28a4a0fc636cc6d021879288
Yifan Zhu <zhuyi@tcd.ie>
2020-07-07 22:30:22 +0100

    Create 1013  Partition Array Into Three Parts With Equal Sum.java

---
+++ All problems and Answers/1013  Partition Array Into Three Parts With Equal Sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canThreePartsEqualSum(int[] A) {
+        int sum = Arrays.stream(A).sum(), part = 0, average = sum / 3, cnt = 0;
+        for (int a : A) {
+            part += a;
+            if (part == average) { // find the average: sum / 3
+                ++cnt; // find an average, increase the counter.
+                part = 0; // reset part.
+            }
+        }
+        return cnt >= 3 && sum % 3 == 0;
+    }
+}
