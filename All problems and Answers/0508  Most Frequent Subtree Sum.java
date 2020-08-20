5aa441ba12cfe031265ef0263403d9c16c20ddae
Yifan Zhu <zhuyi@tcd.ie>
2020-08-20 12:46:35 +0100

    Create 0508  Most Frequent Subtree Sum.java

---
+++ All problems and Answers/0508  Most Frequent Subtree Sum.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+    int maxCount = 0;
+
+    public int[] findFrequentTreeSum(TreeNode root) {
+        dfs(root);
+        List<Integer> res = new ArrayList<>();
+        for (int s : count.keySet()) {
+            if (count.get(s) == maxCount)
+                res.add(s);
+        }
+        return res.stream().mapToInt(i->i).toArray();
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int s = dfs(root.left) + dfs(root.right) + root.val;
+        count.put(s, count.getOrDefault(s, 0) + 1);
+        maxCount = Math.max(maxCount, count.get(s));
+        return s;
+    }
+}
