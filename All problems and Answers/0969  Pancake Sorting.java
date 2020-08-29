d7e115af3d7fe2a06477662c7a4dded9eb5dca76
Yifan Zhu <zhuyi@tcd.ie>
2020-08-29 11:03:30 +0100

    Update 0969  Pancake Sorting.java

--- All problems and Answers/0969  Pancake Sorting.java
+++ All problems and Answers/0969  Pancake Sorting.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<Integer> pancakeSort(int[] A) {
-        List<Integer> ans = new ArrayList();
-        int N = A.length;
-
-        Integer[] B = new Integer[N];
-        for (int i = 0; i < N; ++i)
-            B[i] = i+1;
-        Arrays.sort(B, (i, j) -> A[j-1] - A[i-1]);
-
-        for (int i: B) {
-            for (int f: ans)
-                if (i <= f)
-                    i = f+1 - i;
-            ans.add(i);
-            ans.add(N--);
+        List<Integer> res = new ArrayList<>();
+        for (int x = A.length, i; x > 0; --x) {
+            for (i = 0; A[i] != x; ++i);
+            reverse(A, i + 1);
+            res.add(i + 1);
+            reverse(A, x);
+            res.add(x);
         }
+        return res;
+    }
 
-        return ans;
+    public void reverse(int[] A, int k) {
+        for (int i = 0, j = k - 1; i < j; i++, j--) {
+            int tmp = A[i];
+            A[i] = A[j];
+            A[j] = tmp;
+        }
     }
 }
