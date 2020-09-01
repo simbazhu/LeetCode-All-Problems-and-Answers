8c6860066364c3f31b5d2e107a22f4156553720a
Yifan Zhu <zhuyi@tcd.ie>
2020-09-01 10:13:04 +0100

    Update 0949  Largest Time for Given Digits.java

--- All problems and Answers/0949  Largest Time for Given Digits.java
+++ All problems and Answers/0949  Largest Time for Given Digits.java
@@ -1,21 +1,15 @@
 class Solution {
     public String largestTimeFromDigits(int[] A) {
-        int ans = -1;
-        
-        // Choose different indices i, j, k, l as a permutation of 0, 1, 2, 3
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++) if (j != i)
-                for (int k = 0; k < 4; k++) if (k != i && k != j) {
-                    int l = 6 - i - j - k;
-                    
-                    // For each permutation of A[i], read out the time and
-                    // record the largest legal time.
-                    int hours = 10 * A[i] + A[j];
-                    int mins = 10 * A[k] + A[l];
-                    if (hours < 24 && mins < 60)
-                        ans = Math.max(ans, hours * 60 + mins);
+        String ans = "";
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                for (int k = 0; k < 4; ++k) {
+                    if (i == j || i == k || j == k) continue; // avoid duplicate among i, j & k.
+                    String h = "" + A[i] + A[j], m = "" + A[k] + A[6 - i - j - k], t = h + ":" + m; // hour, minutes, & time.
+                    if (h.compareTo("24") < 0 && m.compareTo("60") < 0 && ans.compareTo(t) < 0) ans = t; // hour < 24; minute < 60; update result.
                 }
-        
-        return ans >= 0 ? String.format("%02d:%02d", ans / 60, ans % 60) : "";
+            }
+        }
+        return ans;
     }
 }
