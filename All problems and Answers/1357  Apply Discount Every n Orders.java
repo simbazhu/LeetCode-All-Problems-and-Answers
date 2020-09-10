a8c451c318d24add7fc10579b16fd85e2346b574
Yifan Zhu <zhuyi@tcd.ie>
2020-09-10 10:38:36 +0100

    Create 1357  Apply Discount Every n Orders.java

---
+++ All problems and Answers/1357  Apply Discount Every n Orders.java
@@ -0,0 +1,24 @@
+class Cashier {
+
+    private int cnt = 0, n, discount;
+    private Map<Integer, Integer> price = new HashMap<>();
+    public Cashier(int n, int discount, int[] products, int[] prices) {
+        this.n = n;
+        this.discount = discount;
+        for (int i = 0; i < products.length; ++i)
+            price.put(products[i], prices[i]);
+    }
+    
+    public double getBill(int[] product, int[] amount) {
+        double total = 0.0d;
+        for (int i = 0; i < product.length; ++i)
+            total += price.get(product[i]) * amount[i];
+        return total * (++cnt % n == 0 ? 1 - discount / 100d : 1);
+    }
+}
+
+/**
+ * Your Cashier object will be instantiated and called as such:
+ * Cashier obj = new Cashier(n, discount, products, prices);
+ * double param_1 = obj.getBill(product,amount);
+ */
