9f5acab11afd6e3255e566343f1d10bf01d2e845
Yifan Zhu <zhuyi@tcd.ie>
2020-09-16 16:13:42 +0100

    Create 1492  The kth Factor of n.java

---
+++ All problems and Answers/1492  The kth Factor of n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+        List<Integer> factorList = new ArrayList<>();
+        for (int factor = 1; factor * factor <= n; ++factor) {
+            if (n % factor == 0) {
+                if (factor * factor != n) {
+                    factorList.add(factor);
+                }
+                if (--k == 0) {
+                    return factor;
+                }
+            }
+        }
+        int size = factorList.size();
+        return k > size ? -1 : n / factorList.get(size - k);
+    }
+}
