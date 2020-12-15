71a79122f2e5e0e8c3fbfef261f6526377ea9cb5
Yifan Zhu <zhuyi@tcd.ie>
2020-12-15 22:18:19 +0000

    Update 1089  Duplicate Zeros.java

--- All problems and Answers/1089  Duplicate Zeros.java
+++ All problems and Answers/1089  Duplicate Zeros.java
@@ -1,22 +1,22 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
         int possibleDups = 0;
-        int length_ = arr.length - 1;
+        int length = arr.length - 1;
         
         // Find the number of zeros to be duplicated
         // Stopping when left points beyond the last element in the original array
         // which would be part of the modified array
-        for (int left = 0; left <= length_ - possibleDups; left++) {
+        for (int left = 0; left <= length - possibleDups; left++) {
             
             // Count the zeros
             if (arr[left] == 0) {
                 
                 // Edge case: This zero can't be duplicated. We have no more space,
                 // as left is pointing to the last element which could be included
-                if (left == length_ - possibleDups) {
+                if (left == length - possibleDups) {
                     // For this zero we just copy it without duplication.
-                    arr[length_] = 0;
-                    length_ -= 1;
+                    arr[length] = 0;
+                    length -= 1;
                     break;
                 }
                 possibleDups++;
@@ -24,7 +24,7 @@ class Solution {
         }
         
         // Start backwards from the last element which would be part of new array.
-        int last = length_ - possibleDups;
+        int last = length - possibleDups;
         
         // Copy zero twice, and non zero once.
         for (int i = last; i >= 0; i--) {
