9c66c7aa1e0adec6469aa1ce54c24ad2ea317855
Yifan Zhu <zhuyi@tcd.ie>
2020-12-19 20:15:26 +0000

    Update 0984  String Without AAA or BBB.java

--- All problems and Answers/0984  String Without AAA or BBB.java
+++ All problems and Answers/0984  String Without AAA or BBB.java
@@ -5,12 +5,14 @@ class Solution {
         while (A > 0 || B > 0) {
             boolean writeA = false;
             int L = ans.length();
-            if (L >= 2 && ans.charAt(L-1) == ans.charAt(L-2)) {
-                if (ans.charAt(L-1) == 'b')
+            if (L >= 2 && ans.charAt(L - 1) == ans.charAt(L - 2)) {
+                if (ans.charAt(L - 1) == 'b') {
                     writeA = true;
+                }
             } else {
-                if (A >= B)
+                if (A >= B) {
                     writeA = true;
+                }
             }
 
             if (writeA) {
