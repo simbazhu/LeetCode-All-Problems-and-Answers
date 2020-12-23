66d05835cb154e409704246861a0ae01bbb00a63
Yifan Zhu <zhuyi@tcd.ie>
2020-12-23 14:09:42 +0000

    Update 0461  Hamming Distance.java

--- All problems and Answers/0461  Hamming Distance.java
+++ All problems and Answers/0461  Hamming Distance.java
@@ -1,9 +1,9 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-            int xor = x^y;
+            int xor = x ^ y;
             int res = 0;
-            while(xor!=0) {
-                res+= xor & 1;
+            while (xor != 0) {
+                res += xor & 1;
                 xor >>= 1;
             }
             return res;
