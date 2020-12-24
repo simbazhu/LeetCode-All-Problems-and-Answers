34dcefeff61bdebc2609cc18591fe48b06d45bfc
Yifan Zhu <zhuyi@tcd.ie>
2020-12-24 16:09:47 +0000

    Update 0733  Flood Fill.java

--- All problems and Answers/0733  Flood Fill.java
+++ All problems and Answers/0733  Flood Fill.java
@@ -8,10 +8,10 @@ class Solution {
     public void dfs(int[][] image, int r, int c, int color, int newColor) {
         if (image[r][c] == color) {
             image[r][c] = newColor;
-            if (r >= 1) dfs(image, r-1, c, color, newColor);
-            if (c >= 1) dfs(image, r, c-1, color, newColor);
-            if (r+1 < image.length) dfs(image, r+1, c, color, newColor);
-            if (c+1 < image[0].length) dfs(image, r, c+1, color, newColor);
+            if (r >= 1) dfs(image, r - 1, c, color, newColor);
+            if (c >= 1) dfs(image, r, c - 1, color, newColor);
+            if (r + 1 < image.length) dfs(image, r + 1, c, color, newColor);
+            if (c + 1 < image[0].length) dfs(image, r, c + 1, color, newColor);
         }
     }
 }
