5305d8caedafa0f8e7d1d320a5336576da81c18c
Yifan Zhu <zhuyi@tcd.ie>
2021-02-07 12:17:01 +0000

    Create 1668  Maximum Repeating Substring.java

---
+++ All problems and Answers/1668  Maximum Repeating Substring.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int ans = 1;
+     // while (sequence.indexOf(word.repeat(ans)) >= 0)
+        while (sequence.contains(word.repeat(ans)))
+            ++ans;
+        return ans - 1;
+    }
+}
