d8e6a3d3f8203a078413c203149cddd6bd564a17
Yifan Zhu <zhuyi@tcd.ie>
2021-02-09 15:03:35 +0000

    Create 1678  Goal Parser Interpretation.java

---
+++ All problems and Answers/1678  Goal Parser Interpretation.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < command.length(); ++i) {
+            char c = command.charAt(i);
+            if (c == 'G') {
+                sb.append(c);
+            }else if (c == '(' && command.charAt(i + 1) == ')') {
+                sb.append("o");
+                ++i;
+            }else {
+                sb.append("al");
+                i += 3;
+            }
+        }
+        return sb.toString();
+    }
+}
