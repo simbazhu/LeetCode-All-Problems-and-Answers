1e27a87f335a7ff54b70e54d2e04e662210085f6
Yifan Zhu <zhuyi@tcd.ie>
2021-02-22 16:36:58 +0000

    Create 1736  Latest Time by Replacing Hidden Digits.java

---
+++ All problems and Answers/1736  Latest Time by Replacing Hidden Digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String maximumTime(String time) {
+        char[] a = time.toCharArray();
+        for (int i = 0; i < 5; ++i) {
+            if (a[i] == '?') {
+                if (i == 0) {
+                    a[i] = a[1] <= '3' || a[1] == '?' ? '2' : '1';
+                }else if (i == 1) {
+                    a[i] = a[0] == '2' ? '3' : '9';
+                }else if (i == 3) {
+                    a[i] = '5';
+                }else if (i == 4) {
+                    a[i] = '9';
+                }
+            }
+        }
+        return String.valueOf(a);
+    }
+}
