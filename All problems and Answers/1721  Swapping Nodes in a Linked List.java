b28a90d62058032d5838fb5516a3e3e8f3afa8d5
Yifan Zhu <zhuyi@tcd.ie>
2021-03-17 23:40:34 +0000

    Update 1721  Swapping Nodes in a Linked List.java

--- All problems and Answers/1721  Swapping Nodes in a Linked List.java
+++ All problems and Answers/1721  Swapping Nodes in a Linked List.java
@@ -11,14 +11,14 @@
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
         ListNode n1 = null, n2 = null;
-        for (var p = head; p != null; p = p.next) {
+        for (ListNode p = head; p != null; p = p.next) {
             n2 = n2 == null ? null : n2.next;
             if (--k == 0) {
                 n1 = p;
                 n2 = head;
             }
         }
-        var tmp = n1.val;
+        int tmp = n1.val;
         n1.val = n2.val;
         n2.val = tmp;
         return head;
