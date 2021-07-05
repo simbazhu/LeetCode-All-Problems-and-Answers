245a06b5c42c5140f573790a07a8de88368d0566
Yifan Zhu <zhuyi@tcd.ie>
2021-07-05 12:16:27 +0100

    Create 1869  Longer Contiguous Segments of Ones than Zeros.java

---
+++ All problems and Answers/1869  Longer Contiguous Segments of Ones than Zeros.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkZeroOnes(String s) {
+        int longestZeros = 0, longestOnes = 0, currentZeros = 0, currentOnes = 0;
+        for(int i=0;i<s.length();i++) 
+            if(s.charAt(i) == '0') {
+                longestZeros = Math.max(longestZeros, ++currentZeros);
+                currentOnes = 0;
+            } else {
+                longestOnes = Math.max(longestOnes, ++currentOnes);
+                currentZeros = 0;
+            }
+        return longestOnes > longestZeros;
+    }
+}
